e1602568b359cf0a257fd2558bc7570abc65b118
D Chanpura <chanpura.d@husky.neu.edu>
2019-09-23 19:15:11 -0400

    updated eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [],
+  "env": {
+    "jest": true
+  },
+  "rules": {
+    "arrow-body-style": 2,
+    "class-methods-use-this": "error",
+    "consistent-return": "off",
+    "curly": "error",
+    "func-names": 2,
+    "max-len": ["error", 120],
+    "no-await-in-loop": "off",
+    "no-case-declarations": "error",
+    "no-continue": "error",
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-proto": "error",
+    "no-return-await": "error",
+    "no-underscore-dangle": "off",
+    "no-unused-vars": ["error", { "args": "none" }],
+    "no-shadow": "error",
+    "strict": "off",
+    "valid-typeof": "error",
+    "linebreak-style": ["error", "unix"],
+  }
+};
