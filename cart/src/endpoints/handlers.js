0498e6feacab6ff9a677145624b9b42c9b509221
Alex Balgavy <a.balgavy@gmail.com>
2019-10-09 18:20:57 -0400

    Created endpoints, no data logic yet

--- cart/src/endpoints/handlers.js
+++ cart/src/endpoints/handlers.js
@@ -14,33 +14,104 @@ class Handlers {
     this.model = model || (new Model());
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  addProduct(req, rep) {
+    // If cart exists, add.
+    // Else, create a new cart and add, incl amount.
+    if (!req.payload) {
+      return rep.response("Body cannot be empty.").code(400);
+    }
+
+    logger.debug(`Adding product ${req.payload} to cart ${req.params.id}`);
+
+    // TODO: addProduct data logic
+    return rep.response("TODO: product should be added.").code(201);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  removeProduct(req, rep) {
+    // If cart does not exist, error.
+    // If product not in cart, error.
+    // Else, remove the product.
+
+    if (!req.payload) {
+      return rep.response("Body cannot be empty.").code(400);
+    }
+
+    logger.debug(`Removing product ${req.payload} from cart ${req.params.id}`);
+    // TODO: removeProduct data logic
+    return rep.response("TODO: product should be removed.").code(200);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  changeAmount(req, rep) {
+    // If product not in cart, error.
+    // If cart does not exist, error.
+    // Should we deal with product not in stock?
+
+    if (!req.payload) {
+      return rep.response("Body cannot be empty.").code(400);
+    }
+
+    logger.debug(`Changing amount of product in cart ${req.params.id}`);
+    // TODO: changeAmount data logic
+    return rep.response("TODO: amount should be changed");
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  emptyCart(req, rep) {
+    // If cart does not exist, error
+    logger.debug(`Emptying cart ${req.params.id}`);
+    // TODO: emptyCart data logic
+    return rep.response("TODO: cart should be emptied.").code(200);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  deleteCart(req, rep) {
+    // If cart does not exist, error
+    // Empty the cart.
+    // Delete the cart.
+
+    logger.debug(`Removing cart ${req.params.id}`);
+    // TODO: deleteCart data logic
+    return rep.response("TODO: cart should be deleted.").code(200);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  getProducts(req, rep) {
+    // If cart does not exist, error.
+    logger.debug(`Listing all products in ${req.params.id}`);
+    // TODO: getProducts data logic
+    return rep.response("TODO: should list everything in cart").code(200)
+  }
+
   /**
    * Find an object by its id
    * @param  {Object} req - the request object
    * @param  {Object} rep - the response object
    */
-  findOneById(req, rep) {
-    const id = req.params.id;
-    const reply = rep;
+  // findOneById(req, rep) {
+  //   const id = req.params.id;
+  //   const reply = rep;
 
-    return this.model.findOneById(id)
-      .then((result) => {
-        logger.info(`Get request for ${this.model.resource}`);
-        return reply.response(result).code(200);
-      })
-      .catch((err) => {
-        logger.error(err.message);
-      });
-  }
+  //   return this.model.findOneById(id)
+  //     .then((result) => {
+  //       logger.info(`Get request for ${this.model.resource}`);
+  //       return reply.response(result).code(200);
+  //     })
+  //     .catch((err) => {
+  //       logger.error(err.message);
+  //     });
+  // }
 
-  insert(req, rep) {
-    const data = req.payload;
-    const reply = rep;
+  // insert(req, rep) {
+  //   const data = req.payload;
+  //   const reply = rep;
 
-    return this.model.insert(data)
-      .then(result => reply.response(result).code(201))
-      .catch(err => logger.error(err.message));
-  }
+  //   return this.model.insert(data)
+  //     .then(result => reply.response(result).code(201))
+  //     .catch(err => logger.error(err.message));
+  // }
 }
 
 module.exports = Handlers;
--- cart/src/endpoints/router.js
+++ cart/src/endpoints/router.js
@@ -15,9 +15,121 @@ class Router {
     this.handler = handler || (new Handlers());
   }
 
+  routePost(server) {
+    server.route([
+      {
+        method: 'POST',
+        path: `/cart/{id}/add`,
+        handler: this.handler.addProduct.bind(this.handler),
+        config: {
+          description: `Add a product to the cart.`,
+          tags: ['api', 'cart'],
+          plugins: {
+            'hapi-swagger': {
+              201: { description: 'Product added' },
+              400: { description: 'Bad request (e.g. body empty)' }
+            }
+          }
+        }
+      }
+    ]);
+  }
+
+  routePut(server) {
+    server.route([
+      {
+        method: 'PUT',
+        path: `/cart/{id}/remove`,
+        handler: this.handler.removeProduct.bind(this.handler),
+        config: {
+          description: 'Remove a product from the cart.',
+          tags: ['api', 'cart'],
+          plugins: {
+            'hapi-swagger': {
+              200: { description: 'Product removed'},
+              400: { description: 'Bad request (e.g. body empty)' }
+            }
+          }
+        }
+      },
+      {
+        method: 'PUT',
+        path: '/cart/{id}/empty',
+        handler: this.handler.emptyCart.bind(this.handler),
+        config: {
+          description: 'Empty the cart, removing all products (but does not remove the cart).',
+          tags: ['api', 'cart'],
+          plugins: {
+            'hapi-swagger': {
+              200: { description: 'Cart cleared' },
+              400: { description: 'Bad request.' }
+            }
+          }
+        }
+      },
+      {
+        method: 'PUT',
+        path: '/cart/{id}/change',
+        handler: this.handler.changeAmount.bind(this.handler),
+        config: {
+          description: 'Change the amount of product in the cart.',
+          tags: ['api', 'cart'],
+          plugins: {
+            'hapi-swagger': {
+              200: { description: 'Amount updated' },
+              400: { description: 'Bad request' }
+            }
+          }
+        }
+      }
+    ]);
+  }
+
+  routeGet(server) {
+    server.route([
+      {
+        method: 'GET',
+        path: `/cart/{id}`,
+        handler: this.handler.getProducts.bind(this.handler),
+        config: {
+          description: 'Get all products in a cart.',
+          tags: ['api', 'cart'],
+          plugins: {
+            'hapi-swagger': {
+              200: { description: 'Product listing returned' },
+              400: { description: 'Bad request' }
+            }
+          }
+        }
+      }
+    ]);
+  }
+
+  routeDelete(server) {
+    server.route([
+      {
+        method: 'DELETE',
+        path: '/cart/{id}',
+        handler: this.handler.deleteCart.bind(this.handler),
+        config: {
+          description: 'Clear and delete the cart.',
+          tags: ['api', 'cart'],
+          plugins: {
+            'hapi-swagger': {
+              200: { description: 'Cart deleted' },
+              400: { desription: 'Bad request' }
+            }
+          }
+        }
+      }
+    ])
+  }
+
   route(server) {
-    // this.routeGet(server);
-    // this.routePost(server);
+    this.routeGet(server);
+    this.routePost(server);
+    this.routePut(server);
+    this.routeDelete(server);
   }
 
   // FOR REFERENCE */
