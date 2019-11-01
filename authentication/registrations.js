6acc5d797e25c00dc9ec8126e8ee6e3181522023
D Chanpura <chanpura.d@husky.neu.edu>
2019-11-01 00:27:54 -0400

    added swagger

--- authentication/registrations.js
+++ authentication/registrations.js
@@ -6,19 +6,38 @@
 'use strict';
 
 const HapiFirebaseAuth = require('hapi-firebase-auth');
-const AuthService = require('./auth');
+const AuthService = require('./auth'); // to register the auth plugin
+
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
+const pkg = require('./package');
 
 module.exports = function registrations(config) {
+  const swaggerOptions = {
+    swagger: '2.0',
+    info: {
+            version: pkg.version,
+            title: 'Auth API Documentation',
+        },
+    };
   return {
     // Each string in this array is `require`d during composition
     plugins: [
       HapiFirebaseAuth,
       AuthService,
+      './src/endpoints/auth-token',
       './src/endpoints/memberships',
       './src/endpoints/security-groups',
       './src/endpoints/stores',
       './src/endpoints/user-security-groups',
       './src/endpoints/users',
+      Inert,
+      Vision,
+      {
+          plugin: HapiSwagger,
+          options: swaggerOptions
+      }
     ],
   };
 };
