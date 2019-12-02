341426faad48c0cb1c2c24b5318a559abf94743a
D Chanpura <chanpura.d@husky.neu.edu>
2019-12-02 16:10:34 -0500

    registered swagger for routes

--- checkout/registrations.js
+++ checkout/registrations.js
@@ -5,11 +5,29 @@
 
 'use strict';
 
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
+const pkg = require('./package');
+
 module.exports = function registrations(config) {
+  const swaggerOptions = {
+    swagger: '2.0',
+    info: {
+            version: pkg.version,
+            title: 'Cart API Documentation',
+        },
+    };
   return {
     // Each string in this array is `require`d during composition
     plugins: [
       './src/endpoints',
+      Inert,
+      Vision,
+      {
+          plugin: HapiSwagger,
+          options: swaggerOptions
+      }
     ],
   };
 };
